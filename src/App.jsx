73618ff92767aa80b67f9783e5c6e5a8a27d3934
Kanishk-Heera <kanishkheera@gmail.com>
2026-07-06 00:13:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button, Center } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Center h="100vh">
+      <Button colorPalette="blue">
+        Chakra UI Working
+      </Button>
+    </Center>
+  );
+}
+
+export default App;
---
+++ src/components/ui/provider.jsx
@@ -0,0 +1,8 @@
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ColorModeProvider } from "./color-mode";
+
+export function Provider(props) {
+  return (
+    <ChakraProvider value={defaultSystem}>{props.children}</ChakraProvider>
+  );
+}
